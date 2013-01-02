1b822dd4c3cf06ae9bdbb7e4ba2faa18c10083a4
Cyril Leroux <cyril.leroux@cgi.com>
2013-01-02 16:17:14 +0100

    work on Traduction class

--- Utils/src/Translator.cpp
+++ Utils/src/Translator.cpp
@@ -1,27 +1,34 @@
 #include "stdafx.h"
 #include "Translator.h"
+#include <iostream>
 
 using namespace std;
 
 const string Translator::LANG_EN = "EN";
 const string Translator::LANG_FR = "FR";
 
-unique_ptr<Translator> Translator::m_instance = nullptr;
+Translator* Translator::m_instance = nullptr;
 
-unique_ptr<Translator> Translator::getInstance()
+Translator* Translator::getInstance()
 {
 	if (m_instance == nullptr) {
-		m_instance = unique_ptr<Translator>(new Translator());
+		//m_instance = unique_ptr<Translator>(new Translator());
+		m_instance = new Translator();
 	}
 	return move(m_instance);
 }
 
 Translator::Translator()
 {
+	m_languageKey = LANG_EN;
 }
 
 Translator::~Translator()
 {
+}
+
+void Translator::SetLanguage(const string& languageKey){
+	m_languageKey = languageKey;
 }
 
 /**
@@ -29,7 +36,7 @@ Translator::~Translator()
  * languageKey The key which define the language.
  * dataFile The path of the language file.
  */
-void Translator::AddLanguage(string languageKey, string dataFile)
+void Translator::AddLanguage(const string& languageKey, const string& dataFile)
 {
 	// Add only if dosen't exist
 	if (m_langFiles.find(languageKey) != m_langFiles.end()) {
@@ -37,15 +44,31 @@ void Translator::AddLanguage(string languageKey, string dataFile)
 	}
 
 	m_langFiles.insert(make_pair(languageKey, Lang(dataFile)));
+	cout << "AddLanguage langFiles.size() : " << (int) m_langFiles.size() << endl;
 }
 
-string Translator::GetString(const string& key, const string& languageKey)
+string Translator::GetString(const string& key)
 {
 	for (auto langFile : m_langFiles) {
-		if (langFile.first == languageKey) {
+		if (langFile.first == m_languageKey) {
+			cout << "GetString(const string& key)" << endl;
 			return langFile.second.GetText(key);
 		}
 	}
-	
-	return ""; // TODO: Throw exception here
+	cerr << "langFiles.size() : " << (int) m_langFiles.size() << endl;
+	cerr << "m_languageKey : " << m_languageKey << endl;
+	return ""; // TODO: Throw exception here StringNotFound
+}
+
+string Translator::GetString(const string& key, const string& arg)
+{
+	for (auto langFile : m_langFiles) {
+		if (langFile.first == m_languageKey) {
+			cout << "GetString(const string& key, const string& arg)" << endl;
+			return langFile.second.GetText(key, arg);
+		}
+	}
+	cerr << "langFiles.size() : " << (int) m_langFiles.size() << endl;
+	cerr << "m_languageKey : " << m_languageKey << endl;
+	return ""; // TODO: Throw exception here StringNotFound
 }
--- Utils/src/Translator.h
+++ Utils/src/Translator.h
@@ -10,16 +10,21 @@ class Translator
 public:
 	static const std::string LANG_EN;
 	static const std::string LANG_FR;
-	
+	static Translator* getInstance();
+	//static std::unique_ptr<Translator> getInstance();
+
 	~Translator();
-	static std::unique_ptr<Translator> getInstance();
-	void AddLanguage(std::string languageKey, std::string dataFile);
-	std::string GetString(const std::string& key, const std::string& lang);
+	void SetLanguage(const std::string& languageKey);
+	void AddLanguage(const std::string& languageKey, const std::string& dataFile);
+	std::string GetString(const std::string& key);
+	std::string GetString(const std::string& key, const std::string& arg);
 
 private:
+	static Translator* m_instance;
+	//static std::unique_ptr<Translator> m_instance;
+	
 	Translator();
-
-	static std::unique_ptr<Translator> m_instance;
+	std::string m_languageKey;
 	std::map<std::string, Lang> m_langFiles;
 };
 
