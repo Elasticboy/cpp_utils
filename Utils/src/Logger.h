b56d59c91d60404f0c1908f65cd8490762372339
Cyril Leroux <cyril0leroux@gmail.com>
2013-02-03 00:24:00 +0100

    Modified membership of SEVERITY_LEVELS and SEVERITY_PREFIX

--- Utils/src/Logger.h
+++ Utils/src/Logger.h
@@ -7,16 +7,13 @@ class Logger
 public:
 	Logger(const std::string& filename);
 	
+
 	void setLogFile(const std::string& filename);
 	void debug(const std::string& message);
 	void info(const std::string& message);
 	void warning(const std::string& message);
 	void error(const std::string& message);
 
-	static const int SEVERITY_DEBUG;
-	static const int SEVERITY_INFO;
-	static const int SEVERITY_WARNING;
-	static const int SEVERITY_ERROR;
 
 private:
 	std::string m_logFile;
@@ -27,9 +24,4 @@ private:
 	static const int LOG_SEVERITY;
 	static const int LOG_SEVERITY_CONSOLE;
 	static const std::string LOG_DIR;
-
-	static const std::string PREFIX_DEBUG;
-	static const std::string PREFIX_INFO;
-	static const std::string PREFIX_WARNING;
-	static const std::string PREFIX_ERROR;
 };
