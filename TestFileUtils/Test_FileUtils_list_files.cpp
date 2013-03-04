f5f6a88d4ec3241ab811a7dbd2f833401c4412c7
Cyril Leroux <cyril0leroux@gmail.com>
2013-03-04 19:51:13 +0100

    Test boost implementation of list_files : list_files_boost

---
+++ TestFileUtils/Test_FileUtils_list_files.cpp
@@ -0,0 +1,34 @@
+#include "CppUnitTest.h"
+
+#include "..\Utils\src\FileUtils.h"
+#include <string>
+#include <functional>
+
+#include <iostream>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace TestFileUtils
+{
+	TEST_CLASS(Test_FileUtils_list_files)
+	{
+	public:
+
+		TEST_METHOD(Test_FileUtils_list_files1)
+		{
+			const string dir = FileUtils::get_current_directory();
+			const string root = "C:\\DevSpaces\\VSProjects\\CppUtils\\TestFileUtils\\test";
+
+			auto filesWinAPI	= FileUtils::list_files(root); 
+			auto filesBoost		= FileUtils::list_files_boost(root);
+			
+			Assert::AreEqual(filesWinAPI.size(), filesBoost.size(), L"The sizes of the vectors are not equal.", LINE_INFO());
+
+			for (int i = 0; i < filesWinAPI.size(); i++) {
+				// TODO: Compare
+			}
+			//Assert::AreEqual(expected, actual, L"", LINE_INFO());
+		}
+	};
+}
