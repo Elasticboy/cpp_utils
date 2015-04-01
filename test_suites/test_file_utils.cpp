9684ea46aa82da7c82f2c0df3312f2fc96f8bd72
Cyril Leroux <cleroux@molotov.tv>
2015-04-01 17:50:05 +0200

    Updated test_file_utils.cpp

--- test_suites/test_file_utils.cpp
+++ test_suites/test_file_utils.cpp
@@ -4,7 +4,7 @@
 
 TEST(FileUtils, GetFilename)
 {
-    fs_utils::file file("C:\\toto\\titi\\tata\\bla.exe");
+    fs_utils::file file("C:/toto/titi/tata/bla.exe");
 
     const std::string expectedFilename = "bla.exe";
     auto actualFilename = file.filename();
@@ -13,18 +13,18 @@ TEST(FileUtils, GetFilename)
 
 TEST(FileUtils, GetFilePath)
 {
-    fs_utils::file file("C:\\toto\\titi\\tata\\bla.exe");
+    fs_utils::file file("C:/toto/titi/tata/bla.exe");
 
-    const std::string expectedFullPath = "C:\\toto\\titi\\tata\\bla.exe";
+    const std::string expectedFullPath = "C:/toto/titi/tata/bla.exe";
     auto actualFullPath = file.path();
     ASSERT_EQ(expectedFullPath, actualFullPath) << "FullPath is not as expected.";
 }
 
 TEST(FileUtils, GetParentPath)
 {
-    fs_utils::file file("C:\\toto\\titi\\tata\\bla.exe");
+    fs_utils::file file("C:/toto/titi/tata/bla.exe");
 
-    const std::string expectedPath = "C:\\toto\\titi\\tata\\";
+    const std::string expectedPath = "C:/toto/titi/tata/";
     auto actualPath = file.parent_path();
     ASSERT_EQ(expectedPath, actualPath) << "Path is not as expected.";
 }
