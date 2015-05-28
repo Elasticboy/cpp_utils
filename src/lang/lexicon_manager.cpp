1469a8ab284ceaed09d57d76e0bf3658efec1a08
Cyril Leroux <cyril0leroux@gmail.com>
2015-05-28 23:25:09 +0200

    Fixed git conflict

--- src/lang/lexicon_manager.cpp
+++ src/lang/lexicon_manager.cpp
@@ -1,7 +1,4 @@
-#include "lexicon_manager.h"
-
 #include "logger/console_logger.h"
-#include "../exception/config_exception.h"
 
 using namespace std;
 
@@ -11,11 +8,9 @@ const string lexicon_manager::LANG_FR = "FR";
 
 auto log = ConsoleLogger(DEBUG);
 
-auto logger = ConsoleLogger(DEBUG);
+lexicon_manager *lexicon_manager::instance_ = nullptr;
 
-lexicon_manager* lexicon_manager::instance_ = nullptr;
-
-lexicon_manager* lexicon_manager::instance()
+lexicon_manager *lexicon_manager::instance()
 {
     if (instance_ == nullptr) {
         instance_ = new lexicon_manager();
@@ -36,22 +31,15 @@ lexicon_manager::lexicon_manager() : language_key_(LANG_EN_US) { }
  */
 void lexicon_manager::set_language(const string& language_key)
 {
-<<<<<<< HEAD
     if (lexicons_.find(language_key) != lexicons_.end()) {
         language_key_ = language_key;
         log.Debug("lexicon_manager::setLanguage(), new language \"" + language_key + "\".");
         return;
     }
-=======
-	if (lexicons_.find(language_key) != lexicons_.end()) {
-		language_key_ = language_key;
-		logger.Debug("lexicon_manager::setLanguage(), new language \"" + language_key + "\".");
-		return;
-	}
->>>>>>> 447bae815cc1f3525bf3822945ea326c0ac920fa
 
-    throw config_exception("lexicon_manager::setLanguage()", "Language \"" + language_key +
-                                                             "\" is not part of the list. lexicon_manager has perhaps not been properly initialized.");
+    throw config_exception("lexicon_manager::setLanguage()",
+                           "Language \"" + language_key +
+                           "\" is not part of the list. lexicon_manager has perhaps not been properly initialized.");
 }
 
 /**
@@ -61,7 +49,6 @@ void lexicon_manager::set_language(const string& language_key)
  */
 void lexicon_manager::add_language(const string& language_key, const string& dataFile)
 {
-<<<<<<< HEAD
     // Add only if doesn't exist
     if (lexicons_.find(language_key) != lexicons_.end()) {
         // Key already exists => return.
@@ -72,18 +59,6 @@ void lexicon_manager::add_language(const string& language_key, const string& dat
     // Key does not exist. Add the file
     lexicons_.insert(make_pair(language_key, lexicon(dataFile)));
     log.Debug("lexicon_manager::addLanguage(), Language \"" + language_key + "\" added to the lexicon_manager.");
-=======
-	// Add only if doesn't exist
-	if (lexicons_.find(language_key) != lexicons_.end()) {
-		// Key already exists => return.
-		logger.Debug("lexicon_manager::addLanguage(), Language \"" + language_key + "\" already added.");
-		return;
-	}
-
-	// Key does not exist. Add the file
-	lexicons_.insert(make_pair(language_key, lexicon(dataFile)));
-	logger.Debug("lexicon_manager::addLanguage(), Language \"" + language_key + "\" added to the lexicon_manager.");
->>>>>>> 447bae815cc1f3525bf3822945ea326c0ac920fa
 }
 
 /**/
